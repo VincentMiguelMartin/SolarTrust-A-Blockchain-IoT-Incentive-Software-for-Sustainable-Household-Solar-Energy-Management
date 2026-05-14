96be943a926c02b597c2b0dcce1a4e917a34c143
Vincent Miguel Martin <vincentmiguelmartin@gmail.com>
2026-05-14 17:36:20 +0800

    feat: Add expo-font dependency and configure fonts in app.json

--- CST2/config.ts
+++ CST2/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = (
-  process.env.EXPO_PUBLIC_API_BASE_URL || "http://10.181.160.107:3000"
+  process.env.EXPO_PUBLIC_API_BASE_URL || "https://solartrust-a-blockchain-iot-incentive-pkde.onrender.com"
 ).replace(/\/+$/, "");
 
 // CHANGE THIS TO IPV4 ADDRESS OF YOUR MACHINE IF YOU ARE TESTING ON A REAL DEVICE
